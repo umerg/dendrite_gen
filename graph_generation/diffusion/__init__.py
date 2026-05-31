5e8ac71e07751d59b4f9afe07920953f4bb45bd4
umer <umer.gupta@newgradient.co.uk>
2026-05-31 13:57:27 +0100

    FM implementation

--- graph_generation/diffusion/__init__.py
+++ graph_generation/diffusion/__init__.py
@@ -1,4 +1,5 @@
 from .basic import DenoisingDiffusionModel
 from .edm import EDMDiffusionModel
+from .flow import FlowMatchingModel
 
-__all__ = ["DenoisingDiffusionModel", "EDMDiffusionModel"]
+__all__ = ["DenoisingDiffusionModel", "EDMDiffusionModel", "FlowMatchingModel"]
